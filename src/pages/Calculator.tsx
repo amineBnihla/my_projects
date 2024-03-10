32e81da00cfa725205e2d7273308fc6fea34d230
amineBnihla <bnihla.10@gmail.com>
2024-03-10 20:38:52 +0100

    my projects

---
+++ src/pages/Calculator.tsx
@@ -0,0 +1,15 @@
+export default function Calculator(){
+
+    return(
+        <>
+        <div className="calculator">
+        <div className="cal_output">
+            <div className="cal_prevCalc"></div>
+            <div className="cal_currentCalc"></div>
+        </div>
+        <button>ََََAC</button>
+
+        </div>
+        </>
+    );
+}
